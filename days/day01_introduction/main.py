7995b0b5b8073e0e2548436ecb8c47c31f4933c2
Wassim Amrani <wassim@Wassims-MacBook-Pro.local>
2026-02-26 15:49:04 -0500

    Day 1 done.

---
+++ days/day01_introduction/main.py
@@ -0,0 +1,20 @@
+print("Ronaldo better")
+
+print(3+2)
+print(16-4)
+print(7*2)
+print(18/5)
+print(2**3)
+print(3%2)
+print(3 // 2)
+
+
+print(type(10)) #int
+print(type("united")) #string
+print(type(4.23)) #float
+print(type(1 + 3j)) #complex
+print(type([1, 2, 3])) #list
+print(type({'name' : 'Wassim'}))#dictionnary
+print(type({9.8, 3.14, 2.7}))#tuple
+
+
---
+++ days/day01_introduction/notes.md
@@ -0,0 +1,1 @@
+Understanding basic syntax and operators.

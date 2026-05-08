6301902b99b357b4ed12707a6800dc7d6f9af3c7
Wassim Amrani <wassimamrani@live.fr>
2026-05-08 11:17:06 -0400

    ways to write a list and initialization

---
+++ days/day05_Lists/main.py
@@ -0,0 +1,12 @@
+# 2 ways to create a list
+lst = list()
+print(len(lst))
+
+lst = []
+print(len(lst))
+
+#list with initail values
+
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB']
+print('Web technologies:', web_techs)
+print('Number of web technologies: ', len(web_techs))

3c38962c55fa24d142e112e1727d9b5bbe933f4a
Wassim Amrani <wassimamrani@live.fr>
2026-05-11 13:43:15 -0400

    logical operators implementation

--- days/day09_conditionals/main.py
+++ days/day09_conditionals/main.py
@@ -9,7 +9,7 @@ else:
 #Nested conditions 
 a = 0
 if a > 0:
-    if a % 2:
+    if a % 2 == 0:
         print('A is positive and even integer')
     else:
         print('A is a positive integer')
@@ -17,3 +17,22 @@ elif a == 0:
     print('A is zero')
 else:
     print('A is a negative number')
+
+#If Condition and Logical Operators
+a = 5
+if a > 0 and a % 2 == 0:
+    print('A is a positive and even integer')
+elif a > 0 and a & 2 != 0:
+    print('A is a positive integer')
+elif a == 0:
+    print('A is equal to 0')
+else:
+    print('A is negative')
+
+# If and Or Logical Operators
+user = 'James'
+access_level = 3
+if user == 'admin' or access_level >= 4:
+    print('Access granted!')
+else:
+    print('Access denied!')

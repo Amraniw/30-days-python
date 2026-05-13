76d0a0dd26bb8f4f4a3bb93df29819590ca05ad4
Wassim Amrani <wassimamrani@live.fr>
2026-05-13 14:51:53 -0400

    Spreading

--- days/day17_Exception_handling/main.py
+++ days/day17_Exception_handling/main.py
@@ -33,3 +33,15 @@ def sum_all(*args):
     return s
 print(sum_all(1, 2, 3))
 print(sum_all(1, 2, 3, 4, 5, 6, 7))
+
+#Spreading
+lst_one = [1, 2, 3]
+lst_two = [4, 5, 6, 7]
+lst = [0, *lst_one, *lst_two]
+print(lst) # [0, 1, 2, 3, 4, 5, 6, 7]
+
+country_lst_one = ['Finland', 'Sweden', 'Norway']
+country_lst_two = ['Algeria', 'France']
+countries = [*country_lst_one, *country_lst_two]
+print(countries)
+
